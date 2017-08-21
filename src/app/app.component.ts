d45a282b4d84a539ce8cbda481698a46b6ee7ff5
lon <dove2580@live.com>
2017-08-21 18:45:19 +0800

    Add semantic css and js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: 'lsu-demo',
+  template: `<router-outlet></router-outlet>`
 })
 export class AppComponent {
-  title = 'app';
+
 }
