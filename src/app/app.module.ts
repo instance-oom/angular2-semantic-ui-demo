476b2ee894171121eff9255fa750b22704e0096d
lon <dove2580@live.com>
2017-08-21 18:28:33 +0800

    init code with angular-cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,18 @@
-import { enableProdMode, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
-import { L_SEMANTIC_UI_MODULE } from 'angular2-semantic-ui';
+import { NgModule } from '@angular/core';
 
-import { LSUDemo } from './app';
-import { PAGES } from './pages';
-import { COMPONENTS } from './components';
-import { AppRouts } from './app.routes';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    LSUDemo,
-    ...COMPONENTS,
-    ...PAGES,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
-    L_SEMANTIC_UI_MODULE,
-    RouterModule.forRoot(AppRouts, { useHash: true })
+    AppRoutingModule
   ],
-  providers: [
-  ],
-  bootstrap: [LSUDemo]
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/index.ts
+++
@@ -1,7 +0,0 @@
-import { CodeBlockDirective } from './codeblock';
-
-export * from './codeblock';
-
-export const COMPONENTS: Array<any> = [
-  CodeBlockDirective
-]
